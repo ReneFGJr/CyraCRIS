6e80cd2b96c2f55fc105cb3d77a95cc2732c75fd
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-11-21 16:43:26 -0300

    Update index.php

--- webService/index.php
+++ webService/index.php
@@ -5,40 +5,92 @@
 function auth()
 {
     $ip = $_SERVER['REMOTE_ADDR'];
-    if (substr($ip, 0, 6) == '143.54') {
-        return True;
-    }
-    if (trim($ip) == '54.233.226.131') {
-        return True;
-    }
-    if (substr($ip, 0, 4) == '127.') {
-        return True;
-    }
+
+    if (substr($ip, 0, 6) == '143.54') { return true; }
+    if (trim($ip) == '54.233.226.131') { return true; }
+    if (substr($ip, 0, 4) == '127.') { return true; }
+
     $dt['erro'] = '500';
     $dt['description'] = 'Access from this IP ' . $ip . ' is not authorized';
-    echo json_encode($dt);
+    header("Content-Type: application/json");
+    echo json_encode($dt, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
     exit;
 }
 
 function getFile($id)
 {
     auth();
+
     $filename = 'lattes' . $id . '.zip';
     $dir = '_lattes';
-
     $file = $dir . '/' . $filename;
 
-    $client = new SoapClient("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl");
-    $param = array('id' => $id);
-    $response = $client->__call('getCurriculoCompactado', $param);
-    print_r($response);
-    #$response = base64_decode($response);
-    file_put_contents($file, $response);
-    sleep(0.5);
-    return true;
+    try {
+        // Verifica diretório
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+
+        // Inicia SOAP
+        $client = new SoapClient(
+            "http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl",
+            array('trace' => true, 'exceptions' => true)
+        );
+
+        $param = array('id' => $id);
+
+        // Tenta obter o currículo
+        $response = $client->__call('getCurriculoCompactado', $param);
+
+        // Resposta vazia
+        if (!$response) {
+            throw new Exception("Resposta vazia do servidor CNPq.");
+        }
+
+        // Debug opcional
+        // echo "<pre>RESPONSE:\n" . htmlspecialchars($response) . "</pre>";
+
+        // Salva ZIP
+        file_put_contents($file, $response);
+
+        return array(
+            "status" => "ok",
+            "arquivo" => $file,
+            "id" => $id
+        );
+
+    } catch (SoapFault $e) {
+        // Erro SOAP
+        $erro = array(
+            "erro" => "SOAP_ERROR",
+            "mensagem" => $e->getMessage(),
+            "codigo" => $e->getCode(),
+            "id" => $id
+        );
+
+        header("Content-Type: application/json");
+        echo json_encode($erro, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+        exit;
+    } catch (Exception $e) {
+        // Outros erros
+        $erro = array(
+            "erro" => "GENERAL_ERROR",
+            "mensagem" => $e->getMessage(),
+            "id" => $id
+        );
+
+        header("Content-Type: application/json");
+        echo json_encode($erro, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+        exit;
+    }
 }
 
+/**************************************************/
 
 $id_lattes = "0016615895456187";
-$arquivo = getFile($id_lattes);
-echo "OK";
+$result = getFile($id_lattes);
+
+header("Content-Type: application/json");
+echo json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
