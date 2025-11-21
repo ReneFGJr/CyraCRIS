e92dc68bc8797dd839b127721c66bca77cc20d5f
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-11-21 15:18:13 -0300

    Create app.php

---
+++ webService/app.php
@@ -0,0 +1,24 @@
+<?php
+    /********************************************************* IA */
+    function getFile($id)
+        {
+
+            $filename = 'lattes'.$id.'.zip';
+            $dir = '_lattes';
+
+            $file = $dir.'/'.$filename;
+
+            $client = new SoapClient("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl");
+            $param = array('id'=>$id);
+            $response = $client ->__call('getCurriculoCompactado', $param);
+                print_r($response);
+            #$response = base64_decode($response);
+            file_put_contents($file,$response);
+            sleep(0.5);
+            return true;
+        }
+
+
+$id_lattes = "0016615895456187"
+$arquivo = get_file($id_lattes)
+
