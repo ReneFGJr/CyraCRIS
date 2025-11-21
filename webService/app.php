ea8c8b4206b2164e9da6131f51363237f675f05b
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-11-21 15:27:16 -0300

    Update app.php

--- webService/app.php
+++ webService/app.php
@@ -1,9 +1,28 @@
 <?php
 
 /********************************************************* IA */
+
+function auth()
+{
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if (substr($ip, 0, 6) == '143.54') {
+        return True;
+    }
+    if (trim($ip) == '54.233.226.131') {
+        return True;
+    }
+    if (substr($ip, 0, 4) == '127.') {
+        return True;
+    }
+    $dt['erro'] = '500';
+    $dt['description'] = 'Access from this IP ' . $ip . ' is not authorized';
+    echo json_encode($dt);
+    exit;
+}
+
 function getFile($id)
 {
-
+    auth();
     $filename = 'lattes' . $id . '.zip';
     $dir = '_lattes';
 
