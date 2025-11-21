93a36bd341ede05a3a390e060e50d1608c21e5f1
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-11-21 16:44:51 -0300

    Update index.php

--- webService/index.php
+++ webService/index.php
@@ -1,7 +1,9 @@
 <?php
 
 /********************************************************* IA */
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 function auth()
 {
     $ip = $_SERVER['REMOTE_ADDR'];
